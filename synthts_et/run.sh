0b703cf313d4db9d2142162ce20b319934f7f5b2
ikiissel@gmail.com <ikiissel@gmail.com>
2016-01-04 10:46:24 +0200

    -

--- synthts_et/run.sh
+++ synthts_et/run.sh
@@ -6,8 +6,6 @@
 #   -lexd   [ühestaja sõnastik] 
 #   -m      [hääle nimi, vt kataloogi htsvoices/]
 
-#   näide:
-#   bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -m htsvoices/eki_et_eva003.htsvoice -o valjund.wav -f sisend.txt
 
-bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o tulemus_c002.wav -f in.txt -m htsvoices/eki_et_tnu.htsvoice 
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o tulemus.wav -f in.txt -m htsvoices/eki_et_tnu.htsvoice 
 	
