9f2e8a6fbb1a9015f6ce07402864b3cf884900ec
ikiissel@gmail.com <ikiissel@gmail.com>
2016-01-11 09:32:23 +0200

    -

--- synthts_et/run.sh
+++ synthts_et/run.sh
@@ -8,4 +8,5 @@
 
 
 bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_tnu.wav -f in.txt -m htsvoices/eki_et_tnu.htsvoice
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_eva.wav -f in.txt -m htsvoices/eki_et_eva.htsvoice
 	
