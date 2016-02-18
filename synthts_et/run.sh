ae05e9e046b4de8de61f64ac35ccca7d799fe167
Indrek Kiissel <ikiissel@gmail.com>
2016-02-18 10:50:15 +0200

    -

--- synthts_et/run.sh
+++ synthts_et/run.sh
@@ -7,9 +7,9 @@
 #   -m      [hääle nimi, vt kataloogi htsvoices/]
 #   -r      [kõnetempo, double, 0.01-2.76] 
 
-bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_eva.wav -f in.txt -m htsvoices/eki_et_eva.htsvoice -s -r 1.1
-bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_lis.wav -f in.txt -m htsvoices/eki_et_lis.htsvoice -s -r 1.1
-bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_rna.wav -f in.txt -m htsvoices/eki_et_rna.htsvoice -s -r 1.1
-bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_tnu.wav -f in.txt -m htsvoices/eki_et_tnu.htsvoice -s -r 1.1
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_eva.wav -f in.txt -m htsvoices/eki_et_eva.htsvoice -r 1.1
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_lis.wav -f in.txt -m htsvoices/eki_et_lis.htsvoice -r 1.1
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_rna.wav -f in.txt -m htsvoices/eki_et_rna.htsvoice -r 1.1
+bin/synthts_et -lex dct/et.dct -lexd dct/et3.dct -o out_tnu.wav -f in.txt -m htsvoices/eki_et_tnu.htsvoice -r 1.1
 
 
